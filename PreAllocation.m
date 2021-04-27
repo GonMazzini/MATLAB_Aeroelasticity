04cfea04823750ef97c8cd1bb687af9ad58bb8b4
GonMazzini <gmazzini@itba.edu.ar>
2021-04-27 14:06:55 -0300

    - Define some new variables - Correct contol part (sign <) - Reduce line of codes More stuff.

--- PreAllocation.m
+++ PreAllocation.m
@@ -2,18 +2,18 @@ function [theta, Wy, Wz, Wy_qs, Wz_qs, Wy_int, Wz_int, pn, pt, P1, P2, P3, T1, T
 % Use this function to define the vector sizes.
 %   Output: Relative wind speed, loads matrices, power and thrust vectors.
 
-% theta defines the angle between the blad and the vertical axis.
+% theta defines the angle between the blade and the vertical axis.
 theta = zeros(nt_max, B);
 
 % Relative wind speed.
 % Why it starts with -3 ?
-Wy = -3*ones(length(r),B,nt_max);
-Wz = -3*ones(length(r),B,nt_max);
+Wy = zeros(length(r),B,nt_max);
+Wz = zeros(length(r),B,nt_max);
 
-Wy_qs = -3*ones(length(r),B,nt_max);
-Wz_qs = -3*ones(length(r),B,nt_max);
-Wy_int = -3*ones(length(r),B,nt_max);
-Wz_int = -3*ones(length(r),B,nt_max);
+Wy_qs = zeros(length(r),B,nt_max);
+Wz_qs = zeros(length(r),B,nt_max);
+Wy_int = zeros(length(r),B,nt_max);
+Wz_int = zeros(length(r),B,nt_max);
 
 % Define loads matrices size.
 pn = zeros(length(r),B,nt_max);
